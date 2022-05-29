fd1e68e96fd0e0c50ab94354880b25f98cab51e6
AdrianC <aacuellar14@gmail.com>
2022-05-29 14:35:39 -0700

    Resizer}

--- javascript.js
+++ javascript.js
@@ -1,25 +1,44 @@
-const container = document.querySelector('#Body');
-const gridDimension=16;
-const gridRow=new Array(gridDimension);
+const containerA = document.querySelector('#Head');
+const containerB = document.querySelector('#Body');
+let gridDimension=8;
 
-for(let row=0;row<gridRow.length;row++){
+const theSlider = document.getElementById("myRange");
 
-    gridRow[row] = new Array(gridDimension);
-    const gridCol= gridRow[row];
-    const divRow = document.createElement("div");
-    divRow.className="Row";
-    container.appendChild(divRow);
+createGrid();
 
-    for(let col=0;col<gridCol.length;col++){
-        const div = document.createElement("div");
-        div.className="Box";
-        div.addEventListener('mouseenter', () =>{
-            div.style.backgroundColor="purple";
-        });
-
-        divRow.appendChild(div);
-        gridCol[col]= div;
+theSlider.oninput = function() {
+    while(containerB.lastChild){
+        containerB.removeChild(containerB.lastChild);
     }
+    gridDimension=parseInt(this.value);
+    containerA.textContent=`${gridDimension}x${gridDimension}`;
+    createGrid();
+  }
 
+function createGrid(){
+    let gridRow=new Array(gridDimension);
+
+    for(let row=0;row<gridRow.length;row++){
+
+        gridRow[row] = new Array(gridDimension);
+        const gridCol= gridRow[row];
+        const divRow = document.createElement("div");
+        divRow.className="Row";
+        containerB.appendChild(divRow);
+    
+        for(let col=0;col<gridCol.length;col++){
+            const div = document.createElement("div");
+            div.className="Box";
+            div.addEventListener('mouseenter', () =>{
+                div.style.backgroundColor="purple";
+            });
+    
+            divRow.appendChild(div);
+            gridCol[col]= div;
+        }
+    
+    }
 }
+
+
 
